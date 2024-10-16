98c660c9a0b0a238046a5d2de9a832e1b5e27c49
Joe Chasinga <jo.chasinga@gmail.com>
2024-10-16 19:17:47 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { onMount } from "svelte";
+	let el;
+
+	onMount(async () => {
+	  const { createEditor } = await import("../lib/editor");
+	  createEditor(el);
+	});
+</script>
+
+<div class="rete" bind:this={el} />
+
+
+<style>
+	.rete {
+	  position: relative;
+	  width: 100vw;
+	  height: 100vh;
+	  font-size: 1rem;
+	  text-align: left;
+	  border: 1px solid #cbd0db;
+	}
+
+	:global(body) {
+	  margin: 0;
+	  overflow: hidden;
+	}
+</style>
